331961751141523b297e2c510f01faaca699aba4
pharingWell <sfugle0@gmail.com>
2024-09-04 14:34:41 -0400

    Changed name to Unmask from Uncover

---
+++ Source/Unmask/UnmaskGameMode.h
@@ -0,0 +1,19 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "UnmaskGameMode.generated.h"
+
+UCLASS(minimalapi)
+class AUnmaskGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	AUnmaskGameMode();
+};
+
+
+
