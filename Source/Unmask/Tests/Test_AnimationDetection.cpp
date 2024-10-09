f536dc327590438c9c0cec9e795ac210fe07a9d4
sfugle <sfugle0@gmail.com>
2024-10-09 14:07:43 -0400

    blank files

---
+++ Source/Unmask/Tests/Test_AnimationDetection.cpp
@@ -0,0 +1,10 @@
+#include "Misc/AutomationTest.h"
+
+IMPLEMENT_SIMPLE_AUTOMATION_TEST(Test_AnimationDetection, "Unmask.Unmask.Tests.Test_AnimationDetection",
+                                 EAutomationTestFlags::EditorContext | EAutomationTestFlags::EngineFilter)
+
+bool Test_AnimationDetection::RunTest(const FString& Parameters)
+{
+	// Make the test pass by returning true, or fail by returning false.
+	return true;
+}
