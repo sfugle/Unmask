45f12df82b5e16bb29567f69c8862ee6a7f85800
James Farnsworth <jamescfar@gmail.com>
2024-10-20 21:35:42 -0400

    Deleted MontageDifference. Even if its functionality is eventually needed, it didn't work and anything useful in it is in SequenceDifference.

--- Source/Unmask/Private/Animation/UMAnimationConsumer.cpp
+++ Source/Unmask/Private/Animation/UMAnimationConsumer.cpp
@@ -13,58 +13,6 @@ static TArray<FBoneIndexType> BoneTreeToBoneIndexTypeArray(TArray<int32> BoneTre
 	return BoneIndexType;
 }
 
-float UMAnimationConsumer::MontageDifference(UAnimMontage *X, UAnimMontage *Y, float Fidelity)
-{
-	float Distance = 0;
-	for (double t = 0; t < std::max(X->CalculateSequenceLength(), Y->CalculateSequenceLength()); t += Fidelity)
-	{
-		TArray<FSlotAnimationTrack> XTrack = X->SlotAnimTracks;
-		TArray<FSlotAnimationTrack> YTrack = Y->SlotAnimTracks;
-
-		for (int i = 0; i < XTrack.Num(); i++)
-		{
-			FAnimSegment XSegment = *(XTrack[i].AnimTrack.GetSegmentAtTime(t));
-			FAnimSegment YSegment = *(YTrack[i].AnimTrack.GetSegmentAtTime(t));
-
-			float TF = t;
-
-			UAnimSequenceBase *XSequence = (XSegment.GetAnimationData(t - XSegment.AnimStartTime, TF));
-			UAnimSequenceBase *YSequence = (YSegment.GetAnimationData(t - YSegment.AnimStartTime, TF));
-
-			FCompactPose XPose, YPose;
-			FBlendedCurve XCurve, YCurve;
-			UE::Anim::FStackAttributeContainer XContainer, YContainer;
-			FAnimationPoseData XData = FAnimationPoseData(XPose, XCurve, XContainer);
-			FAnimationPoseData YData = FAnimationPoseData(YPose, YCurve, YContainer);
-			FAnimExtractContext XOutContext = FAnimExtractContext(t - XSegment.AnimStartTime, false, {}, false);
-			FAnimExtractContext YOutContext = FAnimExtractContext(t - YSegment.AnimStartTime, false, {}, false);
-
-#if WITH_EDITOR
-#define LOCTEXT_NAMESPACE "Initialize Data Model"
-			IAnimationDataController& XController = XSequence->GetController();
-			XController.InitializeModel();
-			IAnimationDataController& YController = YSequence->GetController();
-			YController.InitializeModel();
-#endif
-#undef LOCTEXT
-			
-			XSequence->GetAnimationPose(XData, XOutContext);
-			YSequence->GetAnimationPose(YData, YOutContext);
-
-			TArray<FTransform> XBones = XData.GetPose().GetBoneContainer().GetRefPoseArray();
-			TArray<FTransform> YBones = YData.GetPose().GetBoneContainer().GetRefPoseArray();
-
-			for (int j = 0; j < XBones.Num(); j++)
-			{
-				FRotator XRotator = XBones[j].Rotator();
-				FRotator YRotator = YBones[j].Rotator();
-				Distance += sqrt(pow(XRotator.Pitch - YRotator.Pitch, 2) + pow(XRotator.Roll - YRotator.Roll, 2) + pow(XRotator.Yaw - YRotator.Yaw, 2));
-			}
-		}
-	}
-	return Distance;
-}
-
 float UMAnimationConsumer::SequenceDifference(UAnimSequence *X, UAnimSequence *Y, float Fidelity)
 {
 	FMemMark Mark(FMemStack::Get());
--- Source/Unmask/Public/Animation/UMAnimationConsumer.h
+++ Source/Unmask/Public/Animation/UMAnimationConsumer.h
@@ -15,8 +15,6 @@ class UNMASK_API UMAnimationConsumer : public UObject
 	GENERATED_BODY()
 
 	public:
-	    UFUNCTION(BlueprintCallable, Category = "Animation|Math")
-		static float MontageDifference(UAnimMontage *X, UAnimMontage *Y, float Fidelity);
 		UFUNCTION(BlueprintCallable, Category = "Animation|Math")
 		static float SequenceDifference(UAnimSequence *X, UAnimSequence *Y, float Fidelity);
 };
