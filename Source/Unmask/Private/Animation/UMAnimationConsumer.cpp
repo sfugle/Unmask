d026e885c69ca995dec2d977650d689217657c68
James Farnsworth <jamescfar@gmail.com>
2024-10-20 21:18:56 -0400

    working animation sequence consumer i think

--- Source/Unmask/Private/Animation/UMAnimationConsumer.cpp
+++ Source/Unmask/Private/Animation/UMAnimationConsumer.cpp
@@ -3,6 +3,16 @@
 
 #include "Animation/UMAnimationConsumer.h"
 
+static TArray<FBoneIndexType> BoneTreeToBoneIndexTypeArray(TArray<int32> BoneTree)
+{
+	TArray<FBoneIndexType> BoneIndexType;
+	for (int32 i = 0; i < BoneTree.Num(); i++)
+	{
+		BoneIndexType.Add(BoneTree[i]);
+	}
+	return BoneIndexType;
+}
+
 float UMAnimationConsumer::MontageDifference(UAnimMontage *X, UAnimMontage *Y, float Fidelity)
 {
 	float Distance = 0;
@@ -57,39 +67,55 @@ float UMAnimationConsumer::MontageDifference(UAnimMontage *X, UAnimMontage *Y, f
 
 float UMAnimationConsumer::SequenceDifference(UAnimSequence *X, UAnimSequence *Y, float Fidelity)
 {
+	FMemMark Mark(FMemStack::Get());
+	
 	float Distance = 0;
-	for (double t = 0; t < std::max(X->GetPlayLength(), Y->GetPlayLength()); t += Fidelity)
-	{
-		FCompactPose XPose = FCompactPose();//, YPose = {};
-		FBlendedCurve XCurve = FBlendedCurve();//, YCurve = {};
-		UE::Anim::FStackAttributeContainer XContainer = UE::Anim::FStackAttributeContainer();//, YContainer = {};
-		FAnimationPoseData XData = FAnimationPoseData(XPose, XCurve, XContainer);
-		//FAnimationPoseData YData = FAnimationPoseData(YPose, YCurve, YContainer);
-		FAnimExtractContext XOutContext = FAnimExtractContext(t, false, {}, false);
-		//FAnimExtractContext YOutContext = FAnimExtractContext(t, false, {}, false);
-		
+
+	USkeleton* XSkeleton = X->GetSkeleton();
+	TArray<int32> XBones;
+	XSkeleton->GetChildBones(0, XBones);
+	FBoneContainer XBoneContainer = FBoneContainer(BoneTreeToBoneIndexTypeArray(XBones), UE::Anim::FCurveFilterSettings(), *XSkeleton);
+	
+	FCompactPose XPose = FCompactPose();
+	XPose.SetBoneContainer(&XBoneContainer);
+	FBlendedCurve XCurve = FBlendedCurve();
+	UE::Anim::FStackAttributeContainer XAttributes = UE::Anim::FStackAttributeContainer();
+	FAnimationPoseData XData = FAnimationPoseData(XPose, XCurve, XAttributes);
+
+	USkeleton* YSkeleton = Y->GetSkeleton();
+	TArray<int32> YBones;
+	YSkeleton->GetChildBones(0, YBones);
+	FBoneContainer YBoneContainer = FBoneContainer(BoneTreeToBoneIndexTypeArray(YBones), UE::Anim::FCurveFilterSettings(), *YSkeleton);
+	
+	FCompactPose YPose = FCompactPose();
+	YPose.SetBoneContainer(&YBoneContainer);
+	FBlendedCurve YCurve = FBlendedCurve();
+	UE::Anim::FStackAttributeContainer YAttributes = UE::Anim::FStackAttributeContainer();
+	FAnimationPoseData YData = FAnimationPoseData(YPose, YCurve, YAttributes);
+	
 #if WITH_EDITOR
 #define LOCTEXT_NAMESPACE "Initialize Data Model"
-		IAnimationDataController& XController = X->GetController();
-		XController.InitializeModel();
-		//IAnimationDataController& YController = Y->GetController();
-		//YController.InitializeModel();
+	IAnimationDataController& XController = X->GetController();
+	XController.InitializeModel();
+	IAnimationDataController& YController = Y->GetController();
+	YController.InitializeModel();
 #endif
 #undef LOCTEXT
+	
+	for (double t = 0; t < std::max(X->GetPlayLength(), Y->GetPlayLength()); t += Fidelity)
+	{
+		X->GetBonePose(XData, FAnimExtractContext(t));
+		Y->GetBonePose(YData, FAnimExtractContext(t));
 		
-		X->GetAnimationPose(XData, XOutContext);
-		//Y->GetAnimationPose(YData, YOutContext);
+		TArray<FTransform> XBonePoses = reinterpret_cast<const TArray<FTransform>&>(XPose.GetBones());
+		TArray<FTransform> YBonePoses = reinterpret_cast<const TArray<FTransform>&>(YPose.GetBones());
 
-		XData.GetPose();
-		//TArray<FTransform> XBones = XPose.GetBoneContainer().GetRefPoseArray();
-		//TArray<FTransform> YBones = YData.GetPose().GetBoneContainer().GetRefPoseArray();
-
-		/*for (int j = 0; j < XBones.Num(); j++)
+		for (int j = 0; j < XBonePoses.Num(); j++)
 		{
-			FRotator XRotator = XBones[j].Rotator();
-			//FRotator YRotator = YBones[j].Rotator();
-			Distance += sqrt(pow(XRotator.Pitch, 2) + pow(XRotator.Roll, 2) + pow(XRotator.Yaw, 2));
-		}*/
+			FRotator XRotator = XBonePoses[j].Rotator();
+			FRotator YRotator = YBonePoses[j].Rotator();
+			Distance += Fidelity*sqrt(pow(XRotator.Pitch-YRotator.Pitch, 2) + pow(XRotator.Roll-YRotator.Roll, 2) + pow(XRotator.Yaw-YRotator.Yaw, 2));
+		}
 	}
 	return Distance;
 }
